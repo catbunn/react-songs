e6c6c47c0c453b069abafc04164c6e28a6c43b75
SF ♥ <www.neocurry@gmail.com>
2020-11-18 09:59:54 -0600

    First commit

---
+++ src/reducers/index.js
@@ -0,0 +1,22 @@
+import { combineReducers } from 'redux';
+
+const songsReducer = () => {
+	return [
+		{ title:'No Scrubs', duration: '4:05' },
+		{ title: 'All Star', duration: '2:50' },
+		{ title: 'Macarena', duration: '3:15' },
+		{ title: 'I Want it That Way', duration: '1:45' },
+	];
+};
+
+const selectedSongReducer = (selectedSong=null, action) => {
+	if (action.type === 'SONG_SELECTED') {
+		return action.payload;
+	}
+		return selectedSong;
+};
+
+export default combineReducers({
+	songs: songsReducer,
+	selectedSong : selectedSongReducer
+});
